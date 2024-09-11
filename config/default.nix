d82f669a6b12c8c073b9d13e2b79f2063a78793a
Jonas <git@finkler.me>
2024-09-11 12:08:50 +0200

    initial commit

---
+++ config/default.nix
@@ -0,0 +1,104 @@
+{lib, pkgs, ...}: {
+  imports = [ 
+  ];
+
+  clipboard = {
+    register = "unnamedplus"; # put yanked text into system clipboard
+    providers = {
+      wl-copy = {
+        enable = true;
+        package = pkgs.wl-clipboard;
+      };
+    };
+
+  };
+
+  keymaps = [
+    { # escape insert mode with jj
+      mode = "i";
+      key = "jj";
+      action = "<Esc>";
+    }
+  ];
+
+  opts = {
+    signcolumn = "yes:1";
+    expandtab = true;
+    smarttab = true;
+    shiftwidth = 2;
+    tabstop = 2;
+    softtabstop = 2;
+    mouse = "a";
+    number = true;
+    encoding = "utf-8";
+    fileencoding = "utf-8";
+    smartcase = true; 
+    cursorline = true; # highlight current line
+    hlsearch = true; # highlight search results
+    ignorecase = true; # ignore case while searching
+    incsearch = true; # make search act like in modern browsers
+    ruler = true; # always show cursor
+    autoindent = true;
+    smartindent = true;
+    cindent = true;
+    # wrap = false; # don't wrap text
+    breakindent = true; # indent wrapped text (https://stackoverflow.com/questions/1204149/smart-wrap-in-vim)
+    showbreak = ">>"; # highlight wrapped text
+    # copyindent = true; 
+    showmode = false; # dont show mode
+    cmdheight = 0; # more space for command line
+    scrolloff = 8; # keep 8 lines above/below cursor
+
+    # fortran stuff ... haven't tested if this helps
+    # fortran_free_source = 1;
+    # fortran_do_enddo = 1;
+
+  };
+
+  colorschemes.onedark = {
+    enable = true;
+    settings.style = "warmer";
+  };
+
+  plugins = {
+
+    indent-blankline.enable = true;
+
+    gitsigns.enable = true;
+
+    treesitter = {
+      enable = true; # default with all grammar
+      settings = {
+        indent.enable = true;
+      };
+    };
+
+    lsp = {
+      enable = true;
+      servers = {
+        nil-ls.enable = true; # nix
+        ltex = { # latex
+          enable = true;
+          settings.language = "en-US";
+        };
+        pyright.enable = true; # python
+        clangd.enable = true; # C/C++
+        fortls.enable = true; # fortran
+        jsonls.enable = true; # json
+        yamlls.enable = true; # yaml
+      };
+    };
+    # cmp autocomplete
+    cmp.enable = true;
+    cmp-cmdline.enable = true;
+    cmp-treesitter.enable = true;
+    cmp-path.enable = true;
+    cmp-buffer.enable = true;
+    cmp-nvim-lsp.enable = true;
+    cmp-vim-lsp.enable = true;
+    cmp-vsnip.enable = true;
+    cmp-zsh.enable = true;
+    # lsp-signature.enable = true; # show function signature (plugin does not exist?)
+  };
+
+}
---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "My personal nixvim configuration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    nixvim.url = "github:nix-community/nixvim";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, nixvim }:
+    flake-utils.lib.eachDefaultSystem (system: 
+      let 
+        pkgs = import nixpkgs { 
+          inherit system; 
+        };
+
+        nixvim' = nixvim.legacyPackages.${system};
+        nixvimLib = nixvim.lib.${system};
+        nixvimModule = {
+          inherit pkgs;
+          module = import ./config;
+        };
+        nvim = nixvim'.makeNixvimWithModule nixvimModule;
+      in {
+        
+        packages.default = nvim;
+
+        checks.default = nixvimLib.check.mkTestDerivationFromNixvimModule nixvimModule;
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            nvim
+          ];
+
+          shellHook = ''
+            # remember that we're in a dev env
+            export FLAKE="nixvim"
+            # back to zsh
+            exec zsh
+          '';
+        };
+
+      }
+  );
+}
